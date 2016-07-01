2016f212e201f3f09024aad54f757184c81a9ba6
Phillip Daduna <ph.daduna@outlook.de>
2016-07-01 16:47:27 +0200

    testing

--- Scrabble.java
+++ Scrabble.java
@@ -1,77 +1,70 @@
 
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Scrabble {
-	
-	public int hash(String word){
-		int hash = 7;
-		for(int i=0;i<word.length();i++){
-			hash = hash*31 + word.charAt(i);
-		}
-		return hash%1000+1;
-	}
-	
 
-	@SuppressWarnings({ "deprecation", "unchecked", "rawtypes" })
-	public static void main(String[] args) {
+	private ArrayList<String> lineList;
+
+	public int hash(String word) {
+		int hash = 7;
+		char[] letters = word.toLowerCase().toCharArray();
+		Arrays.sort(letters);
+		for (int i = 0; i < word.length(); i++) {
+			hash = hash * 31 + letters[i];
+		}
+//		hash = hash%;
+//		if(hash==0)
+//			hash++;
+		return hash % 300+1;
+	}
+
+	public static void main(String[] args) throws IOException {
 		Scrabble scrab = new Scrabble();
 		HashTable hashT = new HashTable();
 		
-	    File file = new File("/Users/Duni/Documents/workspace/Scrabble Cheater, Basic Edition/src/words.txt");
-	    FileInputStream fis = null;
-	    BufferedInputStream bis = null;
-	    DataInputStream dis = null;
-	 
-	    System.out.println(scrab.hash("aajv"));
-	    System.out.println(scrab.hash("jaav"));
-	    
-	    
-	    try {
-	      fis = new FileInputStream(file);
-	 
-	      // Here BufferedInputStream is added for fast reading.
-	      bis = new BufferedInputStream(fis);
-	      dis = new DataInputStream(bis);
-	 
-	      while(dis.readLine()!=null){
-	    	  hashT.put(hashT.hash(dis.readLine()), dis.readLine());
-	      }
-	      hashT.display();
-	      
-	      int bla = 0;
-	      // dis.available() returns 0 if the file does not have more lines.
-	      while (dis.available() != 0) {
-	    	  if(scrab.hash(dis.readLine())==0){
-	    		  System.out.println("AAAAAAA");
-	    		  break;
-	    	  }
-	    	 hashT.put(scrab.hash(dis.readLine()),dis.readLine());
-	      // this statement reads the line from the file and print it to
-	        // the console.
-	        System.out.print(dis.readLine());
-	        System.out.println("    " + hashT.hash(dis.readLine()));
-	        if(bla < scrab.hash(dis.readLine())){
-	        	bla = scrab.hash(dis.readLine());
-	        }
+		scrab.readFileLines(
+				"/Users/Duni/Documents/workspace/Scrabble Cheater, Basic Edition/src/Scrabble-Cheater--Basic/words.txt");
 
-		      System.out.println(bla);
-	      }
-	      // dispose all the resources after using them.
-	      fis.close();
-	      bis.close();
-	      dis.close();
-	 
-	    } catch (FileNotFoundException e) {
-	      e.printStackTrace();
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }
-	  }
+		
+		hashT.put(scrab.hash("vatu"), "vatu");
+		System.out.println(scrab.hash("vatu"));
+		System.out.println(hashT.table[96].toStrings());
+		hashT.put(scrab.hash("java"), "java");
+		System.out.println(hashT.table[96].toStrings());
+		System.out.println(hashT.table[96].getNext().toStrings());
+		
 
+		System.out.println(scrab.lineList);
+
+		for (String word : scrab.lineList) {
+			hashT.put(scrab.hash(word), word);
+		}
+
+//		hashT.display();
+		
+		System.out.println(hashT.table[100].toStrings());
+		
+		System.out.println();
+		for(int i=1;i<hashT.table.length;i++){
+			System.out.println(hashT.table[i].toStrings());
+		}
+		
+		
+		
 	}
+
+	private void readFileLines(String textFile) throws IOException {
+		FileReader fr = new FileReader(textFile);
+		BufferedReader br = new BufferedReader(fr);
+		lineList = new ArrayList<String>();
+		String thisLine;
+		while ((thisLine = br.readLine()) != null) {
+			lineList.add(thisLine);
+		}
+		br.close();
+	}
+}

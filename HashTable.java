32eaf6bd3a6a5e42b3ac52879dcb63296cfaa3d2
Phillip Daduna <ph.daduna@outlook.de>
2016-07-03 23:31:56 +0200

    nearly final version

--- HashTable.java
+++ HashTable.java
@@ -1,77 +1,70 @@
 
-
 public class HashTable {
+
 	private final static int TABLE_SIZE = 2387;
-	 
-    Entry[] table;
 
-    HashTable() {
-          table = new Entry[TABLE_SIZE];
-          for (int i = 0; i < TABLE_SIZE; i++)
-                table[i] = null;
-    }
+	Entry[] table;
 
-    public String get(int key) {
-          if (table[key] == null)
-                return null;
-          else {
-                Entry entry = table[key];
-                while (entry != null && entry.getKey() != key)
-                      entry = entry.getNext();
-                	
-                if (entry == null)
-                      return null;
-                else
-                      return entry.getValue();
-          }
-    }
+	HashTable() {
+		table = new Entry[TABLE_SIZE];
+		for (int i = 0; i < TABLE_SIZE; i++)
+			table[i] = null;
+	}
 
-    public void put(int key, String value) {
-       
-          if (table[key] == null)
-                table[key] = new Entry(key, value);
-          else {
-                Entry entry = table[key];
-                while (entry.getNext() != null && entry.getKey() == key){
-                      entry = entry.getNext();
-                }
-                  entry.setNext(new Entry(key, value));
-          }
-    }
+	public String get(int key) {
+		if (table[key] == null)
+			return null;
+		else {
+			Entry entry = table[key];
+			while (entry != null && entry.getKey() != key)
+				entry = entry.getNext();
 
-    public void remove(int key) {
-          if (table[key] != null) {
-                Entry prevEntry = null;
-                Entry entry = table[key];
-                while (entry.getNext() != null && entry.getKey() != key) {
-                      prevEntry = entry;
-                      entry = entry.getNext();
-                }
-                if (entry.getKey() == key) {
-                      if (prevEntry == null)
-                           table[key] = entry.getNext();
-                      else
-                           prevEntry.setNext(entry.getNext());
-                }
-          }
-    }
-    
-    public void display(){
-        
-        for(int i=0;i<TABLE_SIZE;i++){
-        	Entry entry = table[i];
-            if(table[i]!=null){
-            	System.out.print("{"+entry.getKey()+"="+entry.getValue()+"}" +" ");
-//                   while(entry.getValue()!=null){
-//                         System.out.print("{"+entry.getKey()+"="+entry.getValue()+"}" +" ");
-//                         entry=entry.getNext();
-//                   }
-            }
-        }             
-     
-     }
+			if (entry == null)
+				return null;
+			else
+				return entry.getValue();
+		}
+	}
 
-    
-    
-    
+	public void put(int key, String value) {
+
+		if (table[key] == null)
+			table[key] = new Entry(key, value);
+		else {
+			Entry entry = table[key];
+			while (entry.getNext() != null && entry.getKey() == key) {
+				entry = entry.getNext();
+			}
+			entry.setNext(new Entry(key, value));
+		}
+	}
+
+	public void remove(int key) {
+		if (table[key] != null) {
+			Entry prevEntry = null;
+			Entry entry = table[key];
+			while (entry.getNext() != null && entry.getKey() != key) {
+				prevEntry = entry;
+				entry = entry.getNext();
+			}
+			if (entry.getKey() == key) {
+				if (prevEntry == null)
+					table[key] = entry.getNext();
+				else
+					prevEntry.setNext(entry.getNext());
+			}
+		}
+	}
+
+	public void display() {
+
+		for (int i = 0; i < TABLE_SIZE; i++) {
+			Entry entry = table[i];
+			if (table[i] != null) {
+				System.out.print("{" + entry.getKey() + "=" + entry.getValue() + "}" + " ");
+			}
+		}
+
+	}
+
 }
--- Scrabble.java
+++ Scrabble.java
@@ -4,14 +4,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.ListIterator;
 
 public class Scrabble {
 	private ArrayList<String> lineList;
-
-	public Scrabble() {
-
-	}
 
 	public int hash(String word) {
 		int hash = 7;
@@ -20,10 +15,8 @@ public class Scrabble {
 		for (int i = 0; i < word.length(); i++) {
 			hash = hash * 31 + letters[i];
 		}
-		// hash = hash%;
-		// if(hash==0)
-		// hash++;
-		return hash % 300 + 1;
+		hash = (hash < 0) ? hash * -1 : hash;
+		return hash % 123 + 1;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -31,33 +24,13 @@ public class Scrabble {
 		HashTable hashT = new HashTable();
 
 		scrab.readFileLines(
-				"/Users/Duni/Documents/workspace/Scrabble Cheater, Basic Edition/src/Scrabble-Cheater--Basic/words.txt");
-
-//		hashT.put(scrab.hash("vatu"), "vatu");
-//		System.out.println(scrab.hash("vatu"));
-//		System.out.println(hashT.table[96].toStrings());
-//		hashT.put(scrab.hash("java"), "java");
-//		hashT.put(scrab.hash("boob"), "boob");
-//		System.out.println(hashT.table[96].toStrings());
-//		System.out.println(hashT.table[96].getValue());
-//		System.out.println(hashT.table[96].getNext().getNext().getValue());
-
-		// System.out.println(scrab.lineList);
+				"/Users/Duni/Documents/workspace/Scrabble Cheater, Basic Edition/src/Scrabble-Cheater--Basic/7words.txt");
 
 		for (String word : scrab.lineList) {
 			hashT.put(scrab.hash(word), word);
 		}
 
-		// hashT.display();
-		//
-		// System.out.println(scrab.lineList.size());
-		// System.out.println();
-		// for(int i=1;i<hashT.table.length;i++){
-		// if(hashT.table[i].toStrings()==null) break;
-		// else System.out.println(hashT.table[i].toStrings());
-		// }
-
-		System.out.println(scrab.lookUp(hashT.table, "oobb"));
+		System.out.println(scrab.lookUp(hashT.table, "coolhal"));
 
 	}
 
@@ -71,7 +44,8 @@ public class Scrabble {
 		}
 		br.close();
 	}
-	//	if loot = null then there is no match
+
+	// if loot = null then there is no match
 	private String lookUp(Entry[] table, String word) {
 		int hash = hash(word);
 		char[] letters = word.toLowerCase().toCharArray();
@@ -79,20 +53,27 @@ public class Scrabble {
 		String loot = null;
 		Entry entry = table[hash];
 		System.out.println("Your letters are: " + word);
-		while (entry.getNext() != null) {
-			char[] findWord = entry.getValue().toLowerCase().toCharArray();
-			Arrays.sort(findWord);
-			if (Arrays.equals(letters, findWord)) {
-				loot = entry.getValue();
-				System.out.print("You could use this word: ");
-				return loot;
-			} else {
-				entry = entry.getNext();
-				findWord = entry.getValue().toLowerCase().toCharArray();
+		if (table[hash] == null) {
+			loot = "No match found!";
+			return loot;
+		} else {
+			while (entry.getValue() != null) {
+				char[] findWord = entry.getValue().toLowerCase().toCharArray();
 				Arrays.sort(findWord);
+				if (Arrays.equals(letters, findWord)) {
+					loot = entry.getValue();
+					loot = "You could use this word: " + entry.getValue();
+					return loot;
+				} else {
+					if (entry.getNext() == null)
+						break;
+					entry = entry.getNext();
+					findWord = entry.getValue().toLowerCase().toCharArray();
+					Arrays.sort(findWord);
+				}
 			}
 		}
-		System.out.println("No match found!");
+		loot = "No match found!";
 		return loot;
 	}
 
